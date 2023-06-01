fa1a32c7cc08863deda9763bcc5b83aa9ee36513
Simon Wuyts <hi@simonwuyts.com>
2023-06-01 15:24:50 +0200

    Improve icon loading

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,5 @@
 <script lang="ts" setup></script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200');
+@import url('https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block');
 </style>
